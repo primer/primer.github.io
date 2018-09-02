da978f1635126ad315ef32b78e61584e48b3676f
Diana Mounter <broccolini@github.com>
2018-09-02 16:52:31 -0400

    update nav to use Box and update links

--- src/Nav.js
+++ src/Nav.js
@@ -1,38 +1,38 @@
 import React from 'React'
-import BoxPro from './BoxPro'
 import NavLink from './NavLink'
-import {Link} from 'primer-react'
+import {Box, Text, Link} from 'primer-react'
 import LinkLight from './LinkLight'
 import Octicon, {MarkGithub} from '@githubprimer/octicons-react'
 
 const Nav = props => (
-  <BoxPro bg='gray.9' py={2}>
+  <Box bg='gray.9' py={3} className="box-shadow-large">
     <div className='d-flex flex-items-center flex-wrap flex-md-nowrap p-responsive'>
-      <LinkLight nounderline pl={2} mt={1} href='https://primer.github.io'>
-        <BoxPro color='blue.2' inline>
-          <Octicon icon={MarkGithub} ariaLabel="GitHub Primer home" size='medium' />
-        </BoxPro>
+      <LinkLight nounderline pl={2} href='/'>
+        <Box color='blue.2' className="d-flex flex-items-center">
+          <Octicon color="blue.2" icon={MarkGithub} ariaLabel="GitHub Primer home" size='medium' />
+          <Text mx={3} color="blue.2" fontSize="2" lineHeight="condensed" >Primer</Text>
+        </Box>
       </LinkLight>
-      <div className='flex-auto pr-3 pr-lg-4 text-right'>
+      <Box pr={[4, 2]} className='flex-auto text-right'>
         <NavLink
-          href='https://styleguide.github.com/primer/'
-          title='Docs'
+        href='https://styleguide.github.com/primer/'
+        title='Style guide'
         />
         <NavLink
-          href='https://github.com/primer/primer#install'
-          title='Install'
+          href='https://spectrum.chat/primer'
+          title='Community'
         />
         <NavLink
-          href='https://primer.github.io/storybook/'
-          title='Storybook'
+          href='https://github.com/primer/'
+          title='Open-source'
         />
         <NavLink
-          href='https://github.com/primer/primer/blob/master/CHANGELOG.md'
-          title='Changelog'
+        href='/team'
+        title='Team'
         />
-      </div>
+      </Box>
     </div>
-  </BoxPro>
+  </Box>
 )
 
 export default Nav
